985e19722a21673cc60865771d46d6825deb78ea
Bilir Goyari <123924837+OmnitriX-7@users.noreply.github.com>
2025-08-25 19:32:20 +0530

    Time: 1427 ms (5.01%) | Memory: 400.4 MB (5.16%) - LeetSync

---
+++ 2678-design-graph-with-shortest-path-calculator/design-graph-with-shortest-path-calculator.cpp
@@ -0,0 +1,51 @@
+class Graph {
+public:
+    vector<vector<vector<int>>> AdjList;
+    int size;
+    Graph(int n, vector<vector<int>>& edges) {
+        AdjList.resize(n);
+        size = n;
+        for (auto i : edges) {
+            int u = i[0], v = i[1], wt = i[2];
+            AdjList[u].push_back({v, wt});
+        }
+    }
+    
+    void addEdge(vector<int> edge) {
+       AdjList[edge[0]].push_back({edge[1],edge[2]});
+    }
+    
+    int shortestPath(int node1, int node2) {
+        vector<int> dist(size, INT_MAX);
+        dist[node1] = 0;
+
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> minh;
+        minh.push({0, node1});
+
+        while (!minh.empty()) {
+            auto curr = minh.top();
+            int x = curr[0], y = curr[1];
+            minh.pop();
+
+            if (x > dist[y]) continue;
+
+            for (auto i : AdjList[y]) {
+                int node = i[0], wt = i[1];
+
+                if (dist[node] > dist[y] + wt) {
+                    dist[node] = dist[y] + wt;
+                    minh.push({dist[node], node});
+                }
+            }
+        }
+
+        return (dist[node2] == INT_MAX) ? -1 : dist[node2];
+    }
+};
+
+/**
+ * Your Graph object will be instantiated and called as such:
+ * Graph* obj = new Graph(n, edges);
+ * obj->addEdge(edge);
+ * int param_2 = obj->shortestPath(node1,node2);
+ */
