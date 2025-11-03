f0d00854af9dcb8fee4202cdeea000d907c1d754
Bilir Goyari <123924837+OmnitriX-7@users.noreply.github.com>
2025-11-03 14:39:42 +0530

    Time: 27 ms (49.01%) | Memory: 26.1 MB (56.95%) - LeetSync

---
+++ 1543-simplified-fractions/simplified-fractions.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<string> simplifiedFractions(int n) {
+        vector<string> res;
+        for (int i = 2; i <= n; i++){
+            for (int j = 1; j < i; j++){
+                if (__gcd(i, j) == 1){
+                    string s = to_string(j) + '/' + to_string(i);
+                    res.push_back(s);
+                }
+            }
+        }
+        return res;
+    }
+};
