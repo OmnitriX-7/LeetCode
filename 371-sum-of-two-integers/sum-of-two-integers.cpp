3a8d2a31fd3fe7ccfee1ac63c53c40024945f0aa
Bilir Goyari <123924837+OmnitriX-7@users.noreply.github.com>
2025-11-10 01:38:17 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (31.41%) - LeetSync

--- 371-sum-of-two-integers/sum-of-two-integers.cpp
+++ 371-sum-of-two-integers/sum-of-two-integers.cpp
@@ -1,6 +1,22 @@
 class Solution {
 public:
     int getSum(int a, int b) {
-        return a+b;
+        unsigned int na = static_cast<unsigned int>(a);
+        unsigned int nb = static_cast<unsigned int>(b);
+
+        unsigned int ans = 0;
+        int carry = 0;
+
+        for (int i = 0; i < 32; i++) {
+            int bit1 = (na >> i) & 1;
+            int bit2 = (nb >> i) & 1;
+
+            int sum = bit1 ^ bit2 ^ carry;
+            if (sum) ans |= (1U << i);
+
+            carry = (bit1 & bit2) | ((bit1 ^ bit2) & carry);
+        }
+
+        return static_cast<int>(ans);
     }
 };
