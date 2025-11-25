5bc5a0ee953ad83183080781bd37afc674b21446
Bilir Goyari <123924837+OmnitriX-7@users.noreply.github.com>
2025-11-25 23:58:38 +0530

    Time: 2162 ms (9.66%) | Memory: 473.1 MB (77.19%) - LeetSync

---
+++ 4045-longest-balanced-subarray-i/longest-balanced-subarray-i.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int longestBalanced(vector<int>& nums) {
+        int n = nums.size();
+        int ans = INT_MIN;
+        for (int i = 0; i < n; i++){
+            set<int> odd;
+            set<int> even;
+            for (int j = i; j < n; j++){
+                if (nums[j] % 2 == 0) even.insert(nums[j]);
+                else odd.insert(nums[j]);
+                if (odd.size() == even.size()) ans = max(ans, j-i+1);
+            }
+        }
+        if (ans == INT_MIN) return 0;
+        return ans;
+    }
+};
