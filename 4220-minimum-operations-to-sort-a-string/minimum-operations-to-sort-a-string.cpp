f86a51cf5f7b993d4acac9ec74aacb85aaf993f4
Bilir Goyari <123924837+OmnitriX-7@users.noreply.github.com>
2026-03-10 00:41:46 +0530

    Time: 88 ms (50.00%) | Memory: 31.5 MB (50.00%) - LeetSync

--- 4220-minimum-operations-to-sort-a-string/minimum-operations-to-sort-a-string.cpp
+++ 4220-minimum-operations-to-sort-a-string/minimum-operations-to-sort-a-string.cpp
@@ -2,35 +2,30 @@ class Solution {
 public:
     int minOperations(string s) {
         int n = s.size();
-        string comp = s;
         
-        sort(comp.begin(),comp.end());
-        if(comp==s) return 0;
+        string sorted_s = s;
+        sort(sorted_s.begin(), sorted_s.end());
+        
+        if (s == sorted_s) return 0;
+        if (n == 2) return -1;
 
-        if(n==2) return -1;
-        
-        int mx = -1; int mn = 1000;
-        for(auto x:s){
-            mx = max(mx,(int)x);
-            mn = min(mn,(int)x);
+        int mn = s[0], mx = s[0];
+        for (char c : s) {
+            mn = min(mn, (int)c);
+            mx = max(mx, (int)c);
         }
 
-        if((int)s[0]==mx && (int)s[n-1]==mn) 
-        {
-            for(int i=1;i<n-1;i++){
-                if((int)s[i]==mn || (int)s[i]==mx) return 2;
+        if (s[0] == mx && s[n - 1] == mn) {
+            for (int i = 1; i < n - 1; i++) {
+                if (s[i] == mn || s[i] == mx) return 2;
             }
             return 3;
         }
-        
-        if((int)s[0]==mn || (int)s[n-1]==mx) return 1;
-        
-        if((int)s[0]==mx) return 2;
 
-        if((int)s[n-1]==mn) return 2;
-
+        if (s[0] == mn || s[n - 1] == mx) {
+            return 1;
+        }
 
         return 2;
     }
-    
 };
