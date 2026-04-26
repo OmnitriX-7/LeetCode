813a9ffb8a59ea632cd34a6c4bc6ae5f35c1bb7c
Bilir Goyari <123924837+OmnitriX-7@users.noreply.github.com>
2026-04-26 14:58:34 +0530

    Time: 488 ms (12.69%) | Memory: 310.9 MB (5.04%) - LeetSync

--- 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.cpp
+++ 1663-detect-cycles-in-2d-grid/detect-cycles-in-2d-grid.cpp
@@ -1,40 +1,53 @@
 class Solution {
 public:
-    int m, n;
-    vector<vector<bool>> visited;
-    
-    bool dfs(vector<vector<char>>& grid, int x, int y, int fromX, int fromY, char target) {
-        visited[x][y] = true;
+    int bfs(vector<vector<char>>& grid, vector<vector<int>>& vis, int i, int j) {
+        int n = grid.size();
+        int m = grid[0].size();
+        char target = grid[i][j];
+        vector<vector<int>> dv = {{0,1}, {0,-1}, {-1,0}, {1,0}};
+        queue<pair<pair<int, int>, pair<int, int>>> q;
+        q.push({{i, j},{-1, -1}});
 
-        int dirs[4][2] = {{0,1}, {1,0}, {0,-1}, {-1,0}};
-        for (auto& dir : dirs) {
-            int nx = x + dir[0];
-            int ny = y + dir[1];
-
-            if (nx < 0 || ny < 0 || nx >= m || ny >= n || grid[nx][ny] != target) continue;
-
-            if (visited[nx][ny]) {
-                // If it's visited and NOT the parent → cycle found
-                if (nx != fromX || ny != fromY) return true;
-            } else {
-                if (dfs(grid, nx, ny, x, y, target)) return true;
-            }
-        }
-        return false;
-    }
-
-    bool containsCycle(vector<vector<char>>& grid) {
-        m = grid.size();
-        n = grid[0].size();
-        visited = vector<vector<bool>>(m, vector<bool>(n, false));
-
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (!visited[i][j]) {
-                    if (dfs(grid, i, j, -1, -1, grid[i][j])) return true;
+        while (!q.empty()) {
+            auto [curr, parent] = q.front();
+            int x = curr.first, y = curr.second;
+            int pr = parent.first, pc = parent.second;
+            q.pop();
+            vis[x][y] = 1;
+            for (int k = 0; k < 4; k++) {
+                int nr = x + dv[k][0];
+                int nc = y + dv[k][1];
+                if (nr >= 0 && nr < n && nc >= 0 && nc < m && grid[nr][nc] == target) {
+                    if (!vis[nr][nc]) {
+                        vis[nr][nc] = 1;
+                        q.push({{nr, nc}, {x, y}});
+                    }
+                    else {
+                        if (!(nr == pr && nc == pc)) {
+                            return 1;
+                        }
+                    }
                 }
             }
         }
+        return 0;
+    }
+
+    bool containsCycle(vector<vector<char>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!vis[i][j]) {
+                    if (bfs(grid, vis, i, j)) {
+                        return true;
+                    }
+                }
+            }
+        }
+
         return false;
     }
 };
