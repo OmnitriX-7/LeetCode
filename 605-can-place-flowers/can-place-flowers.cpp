347563630616bb9e2edd468b3472d74faa91448f
Bilir Goyari <123924837+OmnitriX-7@users.noreply.github.com>
2026-05-15 09:52:16 +0530

    Time: 0 ms (100.00%) | Memory: 24.1 MB (75.87%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.cpp
+++ 605-can-place-flowers/can-place-flowers.cpp
@@ -1,33 +1,20 @@
 class Solution {
 public:
-    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-        int prev = -1;
-        int t = 0;
-        for (int i = 0; i < flowerbed.size(); i++) {
-            if (flowerbed[i] == 1){
-                if (prev == -1){
-                    t += i/2;
-                    prev = i;
-                    continue;
-                }
-                int l = i-prev-1;
-                if (l % 2 == 0){
-                    t += l/2 - 1;
-                }
-                else {
-                    t += l/2;
-                }
-                prev = i;
+    bool canPlaceFlowers(vector<int>& a, int n) {
+        int sz = a.size();
+        int ans = 0, gap = 0, f = 0;
+        for (int i = 0; i < sz; i++) {
+            if (a[i] == 0) {
+                gap++;
+            } else {
+                if (!f) ans += gap/2;
+                else if (gap > 0) ans += (gap-1)/2;
+                gap = 0;
+                f = 1;
             }
         }
-        if (prev != -1)
-            t += (flowerbed.size()-(prev+1))/2;
-
-        else {
-            double f = flowerbed.size();
-            t += ceil(f/(double)2);
-        }
-        if (t >= n) return true;
-        return false;
+        if (!f) ans += (gap+1)/2;
+        else ans += gap/2;
+        return ans >= n;
     }
 };
