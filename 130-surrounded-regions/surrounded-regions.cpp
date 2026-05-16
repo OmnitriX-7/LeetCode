88f7b6e87990b1fe200a4eebed1f385210a0903c
Bilir Goyari <123924837+OmnitriX-7@users.noreply.github.com>
2026-05-17 01:15:59 +0530

    Time: 4 ms (13.85%) | Memory: 18.3 MB (7.19%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,47 +1,46 @@
 class Solution {
 public:
-    void bfs(vector<vector<char>>& board, vector<vector<int>>& visited, int i, int j) {
-        int m = board.size();
-        int n = board[0].size();
+    void bfs(vector<vector<char>>& board, vector<vector<int>>& vis, int i, int j) {
+        int n = board.size();
+        int m = board[0].size();
         queue<pair<int, int>> q;
-        q.push({i,j});
-        visited[i][j] = 1;
-        vector<pair<int, int>> directions = {{1,0}, {-1,0}, {0,1}, {0,-1}};
-        vector<pair<int, int>> region;
-        bool safe = false;
-
+        q.push({i, j});
+        vis[i][j] = 1;
+        int done = 0;
+        vector<vector<int>> dv = {{1,0}, {-1,0}, {0,1}, {0,-1}};
+        vector<pair<int, int>> cors;
         while (!q.empty()) {
             auto [x,y] = q.front();
             q.pop();
-            region.push_back({x,y});
+            cors.push_back({x,y});
 
-            if (x == 0 || y == 0 || x == m-1 || y == n-1) {
-                safe = true;
-            }
+            if (x == 0 || x == n-1 || y == 0 || y == m-1) done = 1;
 
-            for (auto [dx, dy] : directions) {
-                int nx = x + dx, ny = y + dy;
-                if (nx >= 0 && ny >= 0 && nx < m && ny < n && !visited[nx][ny] && board[nx][ny] == 'O') {
-                    q.push({nx, ny});
-                    visited[nx][ny] = true;
+            for (auto& i : dv) {
+                int nr = x + i[0];
+                int nc = y + i[1];
+                if (nr >= 0 && nr < n && nc >= 0 && nc < m && board[nr][nc] == 'O' && !vis[nr][nc]) {
+                    q.push({nr, nc});
+                    vis[nr][nc] = 1;
                 }
             }
         }
-        if (!safe) {
-            for (auto [i,j] : region) {
-                board[i][j] = 'X';
+
+        if (!done) {
+            for (auto& [x,y] : cors) {
+                board[x][y] = 'X';
             }
         }
     }
 
     void solve(vector<vector<char>>& board) {
-        int m = board.size();
-        int n = board[0].size();
-        vector<vector<int>> visited(m, vector<int>(n,0));
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (!visited[i][j] && board[i][j] == 'O') {
-                    bfs(board, visited, i, j);
+        int n = board.size();
+        int m = board[0].size();
+        vector<vector<int>> vis(n, vector<int>(m,0));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] == 'O' && !vis[i][j]) {
+                    bfs(board, vis, i, j);
                 }
             }
         }
