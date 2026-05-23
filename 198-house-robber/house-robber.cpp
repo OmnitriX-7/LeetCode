5ce87d26901256b7e8af929fd63d66f4aa42c5e2
Bilir Goyari <123924837+OmnitriX-7@users.noreply.github.com>
2026-05-23 10:09:35 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (42.79%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -3,7 +3,6 @@ public:
     int rob(vector<int>& nums) {
         int n = nums.size();
         vector<int> dp(n+1,0);
-        dp[0] = 0;
         dp[1] = nums[0];
 
         for (int i = 2; i <= n; i++) {
