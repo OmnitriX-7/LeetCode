7a472244e606e223aa4e94a9f8fe54b78f5af156
Bilir Goyari <123924837+OmnitriX-7@users.noreply.github.com>
2026-05-23 10:23:26 +0530

    Time: 23 ms (64.34%) | Memory: 17.8 MB (92.74%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -4,15 +4,13 @@ public:
         int n = coins.size();
         vector<int> dp(amount+1, INT_MAX);
         dp[0] = 0;
-
-        for (auto& x : coins){
-            for (int j = x; j <= amount; j++){
-                if (dp[j-x] != INT_MAX){
-                    dp[j] = min(dp[j], dp[j-x]+1);
+        for (int i = 1; i <= amount; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i - coins[j] >= 0 && dp[i-coins[j]] != INT_MAX) {
+                    dp[i] = min(dp[i], dp[i-coins[j]]+1);
                 }
             }
         }
-        if (dp[amount] == INT_MAX) return -1;
-        return dp[amount];
+        return dp[amount] == INT_MAX ? -1 : dp[amount];
     }
 };
