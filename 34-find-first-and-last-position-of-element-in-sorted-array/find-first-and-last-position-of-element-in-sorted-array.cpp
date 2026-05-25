df30e35833cf64c89988f9cdf1ab71dcb59cec23
Bilir Goyari <123924837+OmnitriX-7@users.noreply.github.com>
2026-05-25 20:23:26 +0530

    Time: 0 ms (100.00%) | Memory: 17.5 MB (88.05%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,26 +1,14 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        if (nums.empty()) return {-1, -1};
-        int lo = 0, hi = nums.size()-1;
-
-        while (lo < hi) {
-            int mid = lo + (hi-lo)/2;
-
-            if (nums[mid] < target) lo = mid+1;
-            else hi = mid;
+        auto l = lower_bound(nums.begin(), nums.end(), target);
+        
+        if (l == nums.end() || *l != target) {
+            return {-1, -1};
         }
-
-        int low = 0, up = nums.size();
-
-        while (low < up) {
-            int mid = low + (up-low)/2;
-
-            if (nums[mid] <= target) low = mid+1;
-            else up = mid;
-        }
-
-        if (nums[lo] == target) return {lo, up-1};
-        return {-1, -1};
+        
+        auto r = upper_bound(l, nums.end(), target);
+        
+        return {int(l - nums.begin()), int(r - nums.begin()) - 1};
     }
 };
