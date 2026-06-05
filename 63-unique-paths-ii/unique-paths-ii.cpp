4e16c7d31c4f82629123ad4221124219ca2ee9eb
Bilir Goyari <123924837+OmnitriX-7@users.noreply.github.com>
2026-06-05 10:49:11 +0530

    Time: 0 ms (100.00%) | Memory: 11.8 MB (71.44%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -1,35 +1,28 @@
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int m = obstacleGrid.size();
-        int n = obstacleGrid[0].size();
-        vector<vector<int>> ans(m, vector<int>(n, 0));
+        int n = obstacleGrid.size();
+        int m = obstacleGrid[0].size();
+        vector<vector<int>> dp(n, vector<int>(m,0));
 
-        int done = 0;
-        for (int i = 0; i < m; i++) {
-            if (!done) {
-                if (obstacleGrid[i][0] == 0) ans[i][0] = 1;
-                else done = 1;
-            }
-            else break;
+        dp[0][0] = obstacleGrid[0][0] == 0 ? 1 : 0;
+
+        for (int i = 1; i < n; i++) {
+            if (obstacleGrid[i][0] == 1 || dp[i-1][0] == 0) break;
+            dp[i][0] = 1;
         }
-
-        done = 0;
-        for (int i = 0; i < n; i++) {
-            if (!done) {
-                if (obstacleGrid[0][i] == 0) ans[0][i] = 1;
-                else done = 1;
-            }
-            else break;
-        }
-
         for (int i = 1; i < m; i++) {
-            for (int j = 1; j < n; j++) {
-                if (obstacleGrid[i][j] != 1) {
-                    ans[i][j] += ans[i-1][j] + ans[i][j-1];
-                }
+            if (obstacleGrid[0][i] == 1 || dp[0][i-1] == 0) break;
+            dp[0][i] = 1;
+        }
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j < m; j++) {
+                if (obstacleGrid[i][j] == 1) dp[i][j] = 0;
+                else dp[i][j] = dp[i-1][j] + dp[i][j-1];
             }
         }
-        return ans[m-1][n-1];
+
+        return dp[n-1][m-1];
     }
 };
